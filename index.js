bd09c094ba0f8f843a187ea9e3bd7ed7ed425258
Shashank Vishwakarma <68116555+Shashank1902@users.noreply.github.com>
2020-12-10 00:29:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var acc = document.getElementsByClassName("question");
+var i;
+
+for(i=0; i<acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        var answer = this.nextElementSibling;
+
+        if(answer.style.maxHeight) {
+            answer.style.maxHeight = null;
+        } else {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        }
+
+        var arrow = this.childNodes[1];
+        arrow.classList.toggle("flip");
+    });
+}
